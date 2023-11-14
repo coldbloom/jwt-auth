01284a2fe86ad86d043157455ad8872bd20845bc
Konstantin.Kolesnik <kostya_kol333@mail.ru>
2023-11-14 11:19:16 +0300

    first commit11

---
+++ server/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const sequelize = require('./db')
+
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const PORT = process.env.PORT;
+const app = express();
+
+start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`server start on PORT ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
